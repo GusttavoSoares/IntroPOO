eacbae3c5571d33c5c786d2c625cd68bbb8441c8
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-22 17:00:03 -0300

    Aplicação de calcular área e perímetro do quadrado e retangulo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Quadrado q1 = new Quadrado(7.5);
+Quadrado q2 = new Quadrado(4.0);
+
+Console.WriteLine(q1.Lado);
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+Console.WriteLine(q2.Lado);
+Console.WriteLine(q2.Area);
+Console.WriteLine(q2.Perimetro);
